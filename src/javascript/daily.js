e9f9854a3a2dd9ed94a217b1ba60231887811e50
markke78 <56062146+markke78@users.noreply.github.com>
2019-10-15 13:57:51 -0600

    update on 2019/OCT/15/14:00

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,20 +1,20 @@
 const functions ={
- assertEquals : (p1, p2) => {
-    if (p1 === p2) {
-        return true;
-        }else { 
-            console.log("*** the two values are not the same:")
-            console.log("p1-->", p1);
-            console.log("p2-->", p2);
-        return false;
-        
-    }//daily assignment on 2019/OCT/07
-},
+ //daily assignment on 2019/OCT/07
+    assertEquals : (p1, p2) => {
+        if (p1 === p2) {
+            return true;
+            }else { 
+                console.log("*** the two values are not the same:")
+                console.log("p1-->", p1);
+                console.log("p2-->", p2);
+            return false;
+             }
+    },
 
-makeEmailArr: (email) =>{
-    return email[0] + "." + email[1] + "@evolveu.ca";
-}// daily assignment on 2019/OCT/11
-
+// daily assignment on 2019/OCT/11
+    makeEmailArr: (email) =>{
+        return email[0] + "." + email[1] + "@evolveu.ca";
+    },
 
 }
 
