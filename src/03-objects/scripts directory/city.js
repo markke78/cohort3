bebcff88dac2352d960cf9c5033880a034d570fe
markke78 <56062146+markke78@users.noreply.github.com>
2019-11-05 23:43:06 -0700

    created class city

---
+++ src/03-objects/scripts directory/city.js
@@ -0,0 +1,9 @@
+class City {
+  constructor(name, latitude, longitude, population) {
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.population = population;
+    return;
+  }
+}
