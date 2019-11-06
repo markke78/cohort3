f0c6c3ea9dc55440f9eb1f65fbacbf0fc2e03ea7
markke78 <56062146+markke78@users.noreply.github.com>
2019-11-06 16:14:33 -0700

    updated City function and test

--- src/03-objects/scripts directory/city.js
+++ src/03-objects/scripts directory/city.js
@@ -1,9 +1,49 @@
-class City {
+export class City {
   constructor(name, latitude, longitude, population) {
     this.name = name;
     this.latitude = latitude;
     this.longitude = longitude;
     this.population = population;
-    return;
+  }
+
+  show() {
+    return `${this.name} ${this.latitude} ${this.longitude} ${this.population}`;
+  }
+
+  movedIn(num) {
+    if (num > 0) this.population += num;
+    return this.population;
+  }
+
+  movedOut(num) {
+    if (num >= this.population) {
+      return (this.population = 0);
+    } else if (num > 0) {
+      this.population -= num;
+      return this.population;
+    }
+  }
+
+  howBig() {
+    if (this.population > 100000) {
+      return "City";
+    } else if (this.population > 20000) {
+      return "Large Town";
+    } else if (this.population > 1000) {
+      return "Town";
+    } else if (this.population > 100) {
+      return "Village";
+    }
+    return "Hamlet";
+  }
+
+  whichSphere() {
+    if (this.latitude > 0) {
+      return "Northern Hemipnere";
+    } else if (this.latitude < 0) {
+      return "Southern Hemisphere";
+    } else return "Equator";
   }
 }
+
+class Community {}
--- src/03-objects/scripts directory/city.test.js
+++ src/03-objects/scripts directory/city.test.js
@@ -1,1 +1,26 @@
-test("check class city working", () => {});
+import { City } from "./city.js";
+
+test("check class city working", () => {
+  let m = new City("Calgary", "51.049999", "-114.066666", "1,392,609");
+  expect(m.show()).toEqual("Calgary 51.049999 -114.066666 1,392,609");
+});
+
+test("test movedIn working", () => {
+  let m = new City("Calgary", 51.049999, -114.066666, 1392609);
+  expect(m.movedIn(500)).toBe(1393109);
+});
+
+test("test movedOut working", () => {
+  let m = new City("Calgary", 51.049999, -114.066666, 1392609);
+  expect(m.movedOut(500)).toBe(1392109);
+});
+
+test("test howbig working", () => {
+  let m = new City("Calgary", 51.049999, -114.066666, 1392609);
+  expect(m.howBig()).toBe("City");
+});
+
+test("test whichSphere working", () => {
+  let m = new City("Calgary", 51.049999, -114.066666, 1392609);
+  expect(m.whichSphere()).toBe("Northern Hemipnere");
+});
