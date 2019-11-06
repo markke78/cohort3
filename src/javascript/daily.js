053aeb44d383e7061309600152e030fd546f7b69
markke78 <56062146+markke78@users.noreply.github.com>
2019-11-06 13:00:47 -0700

    updated daily for callback

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,202 +1,212 @@
-const functions ={
- //daily assignment on 2019/OCT/29
-   
-    loopReduce: (arrayObj) => {
-        let balance = arrayObj.reduce((acc,cur)=> acc + cur.balance,0);
-            return balance;
-    },
+const functions = {
+  //daily assignment on 2019/NOV/06
+  //create a new array for balances >= 1000 from the staff data.
+  callbackFunc: arrayObj => {
+    return arrayObj.filter(item => {
+      return item.balance >= 1000;
+    });
+  },
 
-    loopAverage:(arrayObj) => {
-        let balance = arrayObj.reduce((acc,cur)=> acc + cur.balance,0)/arrayObj.length;
-        // let staffMoney=[];
-        // let balance=0; 
-        // arrayObj.forEach((item)=>{
-        // staffMoney.push(item.balance);
-        // balance = staffMoney.reduce((acc,cur)=> acc + cur)/staffMoney.length;
-        // });
-        return balance;
-    },
+  //daily assignment on 2019/OCT/29
 
-    //daily assignment on 2019/OCT/25
-    
-    loopStaffMap: (arrayObj) => {
-            let staffEmail = arrayObj.map((item) => {
-            return functions.makeEmailObj(item);
-        });
-        return staffEmail;
-    },
+  loopReduce: arrayObj => {
+    let balance = arrayObj.reduce((acc, cur) => acc + cur.balance, 0);
+    return balance;
+  },
 
-    // loopStaffMap: (arrayObj) => {
-    //     let staffEmail=[];
-    //     arrayObj.map((item)=>{
-    //         staffEmail.push(functions.makeEmailObj(item));
-    //     })
-    //     return staffEmail;
-    // },
+  loopAverage: arrayObj => {
+    let balance =
+      arrayObj.reduce((acc, cur) => acc + cur.balance, 0) / arrayObj.length;
+    // let staffMoney=[];
+    // let balance=0;
+    // arrayObj.forEach((item)=>{
+    // staffMoney.push(item.balance);
+    // balance = staffMoney.reduce((acc,cur)=> acc + cur)/staffMoney.length;
+    // });
+    return balance;
+  },
 
-    loopStaffForEach: (arrayObj) => {
-        let staffEmail=[];
-        arrayObj.forEach((item)=>{
-        staffEmail.push(functions.makeEmailObj(item));
-        });
-        return staffEmail;
-    },
- 
-    //daily assignment on 2019/OCT/24
-    
-    loopStaffOf: (arrayObj) => {
-        let staffEmail=[];
-        for (let b of arrayObj){
-            staffEmail.push(functions.makeEmailObj(b));
-        }
-        return staffEmail;
-    },
+  //daily assignment on 2019/OCT/25
 
-    loopStaffIn: (arrayObj) => {
-        let staffEmail=[];
-        for (let b in arrayObj){
-            staffEmail.push(functions.makeEmailObj(arrayObj[b]));
-        }
-        return staffEmail;
-    },
-    
-    //daily assignment on 2019/OCT/21
+  loopStaffMap: arrayObj => {
+    let staffEmail = arrayObj.map(item => {
+      return functions.makeEmailObj(item);
+    });
+    return staffEmail;
+  },
 
-    loopStaff: (arrayObj) => {
-        let staffEmail=[];
-        arrayObj.forEach(function(item){
-            staffEmail.push(functions.makeEmailObj(item));
-        });
-        return staffEmail;
+  // loopStaffMap: (arrayObj) => {
+  //     let staffEmail=[];
+  //     arrayObj.map((item)=>{
+  //         staffEmail.push(functions.makeEmailObj(item));
+  //     })
+  //     return staffEmail;
+  // },
 
-    },
+  loopStaffForEach: arrayObj => {
+    let staffEmail = [];
+    arrayObj.forEach(item => {
+      staffEmail.push(functions.makeEmailObj(item));
+    });
+    return staffEmail;
+  },
 
-   //daily assigment on 2019/OCT/16
-    // slice
-    mySlice: (fruits) => {
-        var citrus = fruits.slice(1, 3);
-        return citrus;
-    },
+  //daily assignment on 2019/OCT/24
 
-    // splice
-    mySplice: (fruits) => {
-        var citrus = fruits.splice(2, 0, "Lemon", "Kiwi");
-        return citrus;
-    },
-   
-    // forEach
-    myForEach: (numArray) => {
-        const number = numArray;
-        const double = [];
-        const newArray = number.forEach((num) => {
-            double.push(num*2);
-        })
-            return double;
-    },
+  loopStaffOf: arrayObj => {
+    let staffEmail = [];
+    for (let b of arrayObj) {
+      staffEmail.push(functions.makeEmailObj(b));
+    }
+    return staffEmail;
+  },
 
-    // map
-    myMap: (numbers) => {
-        var x = numbers.map(Math.sqrt)
-        return x;
-    },
+  loopStaffIn: arrayObj => {
+    let staffEmail = [];
+    for (let b in arrayObj) {
+      staffEmail.push(functions.makeEmailObj(arrayObj[b]));
+    }
+    return staffEmail;
+  },
 
-    // reduce
-    myReduce: (arr) => {
-        var total= arr.reduce(function(a,b){return a + b});
-        return total;
-    },
-    
-    // filter
-    myFilter: (words)=>{
-        const result = words.filter(word => word.length > 6)
-        return result;
-    },
+  //daily assignment on 2019/OCT/21
 
-    // sort
-    mySort: (fruits) => {
-        fruits.sort();
-        return fruits;
-    },
-      
- 
-    //daily assignment on 2019/OCT/15
-    // Basics
-    // for
-    loopFor: (cars) => {
-        var text =[];
-        for (var i = 0; i < cars.length; i++){
-            text += cars[i] + ",";
-        }
-           return text;
-    },
+  loopStaff: arrayObj => {
+    let staffEmail = [];
+    arrayObj.forEach(function(item) {
+      staffEmail.push(functions.makeEmailObj(item));
+    });
+    return staffEmail;
+  },
 
-    //while
-    loopWhile: (cars) => {
-        var text = [];
-        var j=0;
-        while (j < cars.length ){
-            text += cars[j] + ",";
-            j=j+1;
-            
-        } return text;
-    },
+  //daily assigment on 2019/OCT/16
+  // slice
+  mySlice: fruits => {
+    var citrus = fruits.slice(1, 3);
+    return citrus;
+  },
 
-    //do while
-    loopDoWhile: (mySw) => {
-        var ourArray = [];
-        var k =0
-        do{
-            ourArray.push(k);
-            k++;
-        } while (k <5);
-        return ourArray
-    },
+  // splice
+  mySplice: fruits => {
+    var citrus = fruits.splice(2, 0, "Lemon", "Kiwi");
+    return citrus;
+  },
 
-    // Next Level
-    // for in
-    loopForIn: (myTeam) => {
-        var z;
-        for (z in myTeam) {
-            return z
-        }
-     },
+  // forEach
+  myForEach: numArray => {
+    const number = numArray;
+    const double = [];
+    const newArray = number.forEach(num => {
+      double.push(num * 2);
+    });
+    return double;
+  },
 
-     //for of
-     loopForOf: (myTeam) => {
-        var z;
-        for (z of myTeam) {
-            return z
-        }
-     },
+  // map
+  myMap: numbers => {
+    var x = numbers.map(Math.sqrt);
+    return x;
+  },
 
+  // reduce
+  myReduce: arr => {
+    var total = arr.reduce(function(a, b) {
+      return a + b;
+    });
+    return total;
+  },
 
-     // daily assignment on 2019/OCT/11
-    makeEmailObj: (nameObj) => {
-        let firstName = nameObj.fname.toLowerCase();
-        let lastName = nameObj.lname.toLowerCase();
-        let email = firstName + "." + lastName + "@evolveu.ca";
-        return email;
-    },
+  // filter
+  myFilter: words => {
+    const result = words.filter(word => word.length > 6);
+    return result;
+  },
 
+  // sort
+  mySort: fruits => {
+    fruits.sort();
+    return fruits;
+  },
 
-    // daily assignment on 2019/OCT/09
-    makeEmailArr: (nameArray) =>{
-        return nameArray[0].toLowerCase() + "." + nameArray[1].toLowerCase() + "@evolveu.ca";
-    },
+  //daily assignment on 2019/OCT/15
+  // Basics
+  // for
+  loopFor: cars => {
+    var text = [];
+    for (var i = 0; i < cars.length; i++) {
+      text += cars[i] + ",";
+    }
+    return text;
+  },
 
- 
-    //daily assignment on 2019/OCT/07
-    assertEquals : (p1, p2) => {
-        if (p1 === p2) {
-            return true;
-            }else { 
-                console.log("*** the two values are not the same:")
-                console.log("p1-->", p1);
-                console.log("p2-->", p2);
-            return false;
-             }
-    },
+  //while
+  loopWhile: cars => {
+    var text = [];
+    var j = 0;
+    while (j < cars.length) {
+      text += cars[j] + ",";
+      j = j + 1;
+    }
+    return text;
+  },
 
-}
+  //do while
+  loopDoWhile: mySw => {
+    var ourArray = [];
+    var k = 0;
+    do {
+      ourArray.push(k);
+      k++;
+    } while (k < 5);
+    return ourArray;
+  },
+
+  // Next Level
+  // for in
+  loopForIn: myTeam => {
+    var z;
+    for (z in myTeam) {
+      return z;
+    }
+  },
+
+  //for of
+  loopForOf: myTeam => {
+    var z;
+    for (z of myTeam) {
+      return z;
+    }
+  },
+
+  // daily assignment on 2019/OCT/11
+  makeEmailObj: nameObj => {
+    let firstName = nameObj.fname.toLowerCase();
+    let lastName = nameObj.lname.toLowerCase();
+    let email = firstName + "." + lastName + "@evolveu.ca";
+    return email;
+  },
+
+  // daily assignment on 2019/OCT/09
+  makeEmailArr: nameArray => {
+    return (
+      nameArray[0].toLowerCase() +
+      "." +
+      nameArray[1].toLowerCase() +
+      "@evolveu.ca"
+    );
+  },
+
+  //daily assignment on 2019/OCT/07
+  assertEquals: (p1, p2) => {
+    if (p1 === p2) {
+      return true;
+    } else {
+      console.log("*** the two values are not the same:");
+      console.log("p1-->", p1);
+      console.log("p2-->", p2);
+      return false;
+    }
+  }
+};
 
 export default functions;
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,135 +1,168 @@
-import functions from './daily.js'
+import functions from "./daily.js";
 
-
-test('Compare two parameters', () => {
-    expect(functions.assertEquals("a","b")).toBe(false);
-    expect(functions.assertEquals("a","a")).toBe(true);
-    expect(functions.assertEquals(1,2)).toBe(false);
-    expect(functions.assertEquals(2,2)).toBe(true);
-    expect(functions.assertEquals("2",2)).toBe(false);
-    expect(functions.assertEquals("This value","This value")).toBe(true);
+test("Compare two parameters", () => {
+  expect(functions.assertEquals("a", "b")).toBe(false);
+  expect(functions.assertEquals("a", "a")).toBe(true);
+  expect(functions.assertEquals(1, 2)).toBe(false);
+  expect(functions.assertEquals(2, 2)).toBe(true);
+  expect(functions.assertEquals("2", 2)).toBe(false);
+  expect(functions.assertEquals("This value", "This value")).toBe(true);
 });
 
-test('email builder from an array', () => {
-    const name = ["first", "last"];
-    expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
-    expect(functions.makeEmailArr(["First", "Last"])).toEqual("first.last@evolveu.ca");
-    expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual("bill.smith@evolveu.ca");
+test("email builder from an array", () => {
+  const name = ["first", "last"];
+  expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
+  expect(functions.makeEmailArr(["First", "Last"])).toEqual(
+    "first.last@evolveu.ca"
+  );
+  expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual(
+    "bill.smith@evolveu.ca"
+  );
 });
 
-
-test('email builder from an object / map', () => {
-    const name = { fname: 'first', lname: 'last' };
-    expect(functions.makeEmailObj(name)).toEqual("first.last@evolveu.ca");
-    expect(functions.makeEmailObj({ fname: 'First', lname: 'Last' })).toEqual("first.last@evolveu.ca");
-    expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" })).toEqual("bill.smith@evolveu.ca");
+test("email builder from an object / map", () => {
+  const name = { fname: "first", lname: "last" };
+  expect(functions.makeEmailObj(name)).toEqual("first.last@evolveu.ca");
+  expect(functions.makeEmailObj({ fname: "First", lname: "Last" })).toEqual(
+    "first.last@evolveu.ca"
+  );
+  expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" })).toEqual(
+    "bill.smith@evolveu.ca"
+  );
 });
 
-test('test forLoop', () => {
-    expect(functions.loopFor(["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"])).toEqual("BMW,Volvo,Saab,Ford,Fiat,Audi,");
+test("test forLoop", () => {
+  expect(
+    functions.loopFor(["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"])
+  ).toEqual("BMW,Volvo,Saab,Ford,Fiat,Audi,");
 });
 
-test('test whileLoop', () => {
-    expect(functions.loopWhile(["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"])).toBe("BMW,Volvo,Saab,Ford,Fiat,Audi,");
+test("test whileLoop", () => {
+  expect(
+    functions.loopWhile(["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"])
+  ).toBe("BMW,Volvo,Saab,Ford,Fiat,Audi,");
 });
 
-test('test doWhileLoop', () => {
-    expect(functions.loopDoWhile(5)[3]).toBe(3);
+test("test doWhileLoop", () => {
+  expect(functions.loopDoWhile(5)[3]).toBe(3);
 });
 
-test('test forIn', () => {
-    expect(functions.loopForIn(["Mark", "Ethan", "Jim"][0])).toBe("0");
-})
+test("test forIn", () => {
+  expect(functions.loopForIn(["Mark", "Ethan", "Jim"][0])).toBe("0");
+});
 
-test('test forOf', () => {
-    expect(functions.loopForOf(["Mark", "Ethan", "Jim"][0])).toBe("M");
-})
+test("test forOf", () => {
+  expect(functions.loopForOf(["Mark", "Ethan", "Jim"][0])).toBe("M");
+});
 
-test('test slice', () => {
-    expect(functions.mySlice(["Banana", "Orange", "Lemon", "Apple", "Mango"])).toEqual(["Orange", "Lemon"]);
-})
+test("test slice", () => {
+  expect(
+    functions.mySlice(["Banana", "Orange", "Lemon", "Apple", "Mango"])
+  ).toEqual(["Orange", "Lemon"]);
+});
 
-test('test splice', () => {
-    expect(functions.mySplice(["Banana", "Orange", "Apple", "Mango"])).toEqual([]);
-})
+test("test splice", () => {
+  expect(functions.mySplice(["Banana", "Orange", "Apple", "Mango"])).toEqual(
+    []
+  );
+});
 
-test('test forEach', () => {
-    expect(functions.myForEach([1, 2, 3, 4])).toEqual([2, 4, 6, 8]);
-})    
+test("test forEach", () => {
+  expect(functions.myForEach([1, 2, 3, 4])).toEqual([2, 4, 6, 8]);
+});
 
-test('test map', () => {
-    expect(functions.myMap([4, 9, 16, 25])).toEqual([2, 3, 4, 5]);
-})    
+test("test map", () => {
+  expect(functions.myMap([4, 9, 16, 25])).toEqual([2, 3, 4, 5]);
+});
 
-test('test reduce', () => {
-    expect(functions.myReduce([1, 2, 3, 4])).toBe(10);
-})
+test("test reduce", () => {
+  expect(functions.myReduce([1, 2, 3, 4])).toBe(10);
+});
 
-test('test filter', () => {
-    expect(functions.myFilter(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'])).toEqual(['exuberant', 'destruction', 'present']);
-})
+test("test filter", () => {
+  expect(
+    functions.myFilter([
+      "spray",
+      "limit",
+      "elite",
+      "exuberant",
+      "destruction",
+      "present"
+    ])
+  ).toEqual(["exuberant", "destruction", "present"]);
+});
 
-test('test sort', () => {
-    expect(functions.mySort(["Banana", "Orange", "Apple", "Mango"])).toEqual(["Apple","Banana","Mango","Orange"]);
-})
+test("test sort", () => {
+  expect(functions.mySort(["Banana", "Orange", "Apple", "Mango"])).toEqual([
+    "Apple",
+    "Banana",
+    "Mango",
+    "Orange"
+  ]);
+});
 
-test('email builder for company', () => {
-    const staffEmail = functions.loopStaff(data.staff);
-    expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
-    expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
-    expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
-    })
+test("email builder for company", () => {
+  const staffEmail = functions.loopStaff(data.staff);
+  expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
+  expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
+  expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
+});
 
-    test('email builder for company', () => {
-        const staffEmail = functions.loopStaffOf(data.staff);
-        expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
-        expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
-        expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
-        })
+test("email builder for company", () => {
+  const staffEmail = functions.loopStaffOf(data.staff);
+  expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
+  expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
+  expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
+});
 
-        test('email builder for company', () => {
-            const staffEmail = functions.loopStaffIn(data.staff);
-            expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
-            expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
-            expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
-            })
+test("email builder for company", () => {
+  const staffEmail = functions.loopStaffIn(data.staff);
+  expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
+  expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
+  expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
+});
 
-        test('email builder for company', () => {
-            const staffEmail = functions.loopStaffMap(data.staff);
-            expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
-            expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
-            expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
-            })
+test("email builder for company", () => {
+  const staffEmail = functions.loopStaffMap(data.staff);
+  expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
+  expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
+  expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
+});
 
-        test('email builder for company', () => {
-            const staffEmail = functions.loopStaffForEach(data.staff);
-            expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
-            expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
-            expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
-            })
+test("email builder for company", () => {
+  const staffEmail = functions.loopStaffForEach(data.staff);
+  expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
+  expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
+  expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
+});
 
-        test('email builder for company', () => {
-            expect(functions.loopReduce(data.staff)).toBe(3823);
-            })
+test("email builder for company", () => {
+  expect(functions.loopReduce(data.staff)).toBe(3823);
+});
 
-        test('email builder for company', () => {
-            expect(functions.loopAverage(data.staff)).toBe(546.1428571428571);
-            })
-    
+test("email builder for company", () => {
+  expect(functions.loopAverage(data.staff)).toBe(546.1428571428571);
+});
 
-            
+test("text callback", () => {
+  expect(functions.callbackFunc(data.staff).length).toBe(2);
+  expect(functions.callbackFunc(data.staff)).toEqual([
+    { fname: "Liam", lname: "Henry", balance: 1000 },
+    { fname: "Emma", lname: "Jones", balance: 1330 }
+  ]);
+});
 
-    const data = {
-        staff: [
-            { fname: "Jane", lname: "Smith", balance: 10 },
-            { fname: "Liam", lname: "Henry", balance: 1000 },
-            { fname: "Emma", lname: "Jones", balance: 1330 },
-            { fname: "Olivia", lname: "Notly", balance: 310 },
-            { fname: "Noah", lname: "Ho", balance: 503 },
-            { fname: "William", lname: "Lee", balance: 520 },
-            { fname: "Benjamin", lname: "Amis", balance: 150 },
-        ],
-        company: "EvolveU",
-        city: "Calgary",
-        prov: "Alberta"
-    };
+const data = {
+  staff: [
+    { fname: "Jane", lname: "Smith", balance: 10 },
+    { fname: "Liam", lname: "Henry", balance: 1000 },
+    { fname: "Emma", lname: "Jones", balance: 1330 },
+    { fname: "Olivia", lname: "Notly", balance: 310 },
+    { fname: "Noah", lname: "Ho", balance: 503 },
+    { fname: "William", lname: "Lee", balance: 520 },
+    { fname: "Benjamin", lname: "Amis", balance: 150 }
+  ],
+  company: "EvolveU",
+  city: "Calgary",
+  prov: "Alberta"
+};
