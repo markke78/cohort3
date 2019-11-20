2adad1ea7a2694f5586b452b75d0c3c130b800b2
markke78 <56062146+markke78@users.noreply.github.com>
2019-11-20 00:38:43 -0700

    react-01 done

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import MyComp from "./components/myComponent";
+import EvenComponent from "./components/EvenComponent";
+import OddComponent from "./components/OddComponent";
 
 class App extends React.Component {
   constructor() {
     super();
     this.counter = 21;
+    this.state = { myState: "TBD" };
   }
 
   onPushMe = () => {
     console.log("You pushed me");
+    console.log(this.counter++);
+    this.setState({ myState: "now:" + this.counter });
+  };
+
+  evenOrOdd = () => {
+    if (this.counter % 2 === 0) {
+      return <EvenComponent />;
+    } else return <OddComponent />;
   };
 
   render() {
@@ -20,6 +32,9 @@ class App extends React.Component {
             I am in control of this application and my name is Mark{" "}
             {this.counter}
           </h1>
+          {this.state.myState}
+          <MyComp whatToSay="what Ever" onPushMe={this.onPushMe} />
+          {this.evenOrOdd()}
           <button onClick={this.onPushMe}>Push Me</button>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ react-01/src/EvenComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class EvenComponent extends React.Component {
+  render() {
+    return <h2>Even Comp</h2>;
+  }
+}
+
+export default EvenComponent;
