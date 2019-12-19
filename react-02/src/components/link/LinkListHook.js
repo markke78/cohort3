3dd1369dae3186d1dc9a402aa72f999385cacd4d
markke78 <56062146+markke78@users.noreply.github.com>
2019-12-18 23:22:14 -0700

    add hook page

--- react-02/src/components/link/LinkListHook.js
+++ react-02/src/components/link/LinkListHook.js
@@ -4,7 +4,29 @@ const List = props => {
   // 你可以在這裡使用 Hook！
   return (
     <div>
-      <h1>the LinkList is coming soon</h1>
+      <h1>Linked List</h1>
+      <div className="linkList1">
+        <h5>Subject Name:</h5>
+        <input
+          type="text"
+          // ref={input => {
+          //   this.subjectName = input;
+          // }}
+        />
+        <h5>Number:</h5>
+        <input
+          type="number"
+          // ref={input => {
+          //   this.amount = input;
+          // }}
+        />
+        <button
+          className="submitLL"
+          // onClick={() => this.("")}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
