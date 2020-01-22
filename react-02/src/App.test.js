2509cb00ddc989221b6b1a816fb6ef4cf2df380c
markke78 <56062146+markke78@users.noreply.github.com>
2020-01-21 17:51:13 -0700

    modified the import

--- react-02/src/App.test.js
+++ react-02/src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ReactAccount from "./account";
+import ReactAccount from "./components/account/account";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
