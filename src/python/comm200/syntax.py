25c9e1210f051b47ceb22f6c22b18a97bda53482
markke78 <56062146+markke78@users.noreply.github.com>
2020-02-03 13:16:44 -0700

    update comm200 python syntax

--- src/python/comm200/syntax.py
+++ src/python/comm200/syntax.py
@@ -1,2 +1,60 @@
+    # define attributes / variables
+    #     number
+a=5
+    #     string
+name="Mark"
+    #     boolean
+def myboolean(a, b):
+    if a>b:
+        return True
+    return False
+
+    #     array
+b=[1,2,3,4,5]
+    #     dictionary / objects
+students ={'name':'Mark', 'id':'123456'}
+    #     None
+b= 6
+    # sample if / else
+    
+def if_else(a, b):
+    if a>b:
+        return "a is bigger"
+    else:
+        return "b is bigger"
+
+
+    # functions
+    #     parameters
+    #     returns
+
 def add(x, y):
-    return x+y
+   return x+y
+
+    # lists
+    #     add to the front
+
+def add_to_the_front(item, mylist):
+    mylist.insert(0, item)
+    return mylist
+    
+
+    #     add to the end
+def add_to_the_end(item, mylist):
+    mylist.insert(len(mylist),5)
+    return mylist
+
+    #     update values
+
+    #     comprehension
+    # loops 
+    #     for
+    #     for/in
+    #     while
+    #     do while
+    # Objects / Dictionaries
+    #     declare object
+    #     lookup key to retrieve value
+
+
+
--- src/python/comm200/syntax_test.py
+++ src/python/comm200/syntax_test.py
@@ -1,4 +1,17 @@
 import syntax
+
+def test_myboolean():
+    assert syntax.myboolean(5, 6) == False
+
 def test_add():
     assert syntax.add(2, 3) == 5
     assert syntax.add(0.1, 0.13) == 0.23
+
+def test_add_to_the_front():
+    assert syntax.add_to_the_front(0, [1,2,3,4]) == [0, 1, 2, 3, 4]
+
+def test_add_to_the_end():
+    assert syntax.add_to_the_end(5, [1,2,3,4]) == [1, 2, 3, 4,5]
+
+def test_if_else():
+    assert syntax.if_else(5, 6) == 'b is bigger'
