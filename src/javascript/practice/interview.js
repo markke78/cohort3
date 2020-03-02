268c9589b780208d7f9a1f31ea03625f4734798c
markke78 <56062146+markke78@users.noreply.github.com>
2020-03-01 23:41:50 -0700

    updated practice

--- src/javascript/practice/interview.js
+++ src/javascript/practice/interview.js
@@ -18,6 +18,70 @@ const functions = {
     strs.split("").every((char, i) => {
       return char === str[str.length - i - 1];
     });
+  },
+
+  reverseInt: n => {
+    return (
+      parseInt(
+        n
+          .toString()
+          .split("")
+          .reverse()
+          .join("")
+      ) * Math.sign(n)
+    );
+  },
+
+  maxChar: str => {
+    const charMap = {};
+    let max = 0;
+    let maxChar = "";
+
+    for (let char of str) {
+      if (charMap[char]) {
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+    }
+    for (let char in charMap) {
+      if (charMap[char] > max) {
+        max = charMap[char];
+        maxChar = char;
+      }
+    }
+  },
+
+  fizzBuzz: n => {
+    let myArray = [];
+    for (let b of n) {
+      if (b % 3 === 0 && b % 5 === 0) {
+        myArray.push("fizzBuzz");
+      } else if (b % 3 === 0) {
+        myArray.push("fizz");
+      } else if (b % 5 === 0) {
+        myArray.push("Buzz");
+      } else myArray.push(b);
+    }
+    return myArray;
+  },
+
+  chunk: (array, size) => {
+    const chunked = [];
+
+    for (let element of array) {
+      const last = chunked[chunked.length - 1];
+      console.log(last);
+      if (!last || last.length === size) {
+        chunked.push([element]);
+        console.log(chunked);
+      } else {
+        last.push(element);
+        console.log(last);
+      }
+    }
+
+    return chunked;
   }
 };
 
--- src/javascript/practice/interview.test.js
+++ src/javascript/practice/interview.test.js
@@ -12,3 +12,36 @@ test("palindrome", () => {
 test("palindrome", () => {
   expect(functions.palindrome("abcba")).toBe(true);
 });
+
+test("reverseInt", () => {
+  expect(functions.reverseInt(51)).toBe(15);
+  expect(functions.reverseInt(200)).toBe(2);
+  expect(functions.reverseInt(-15)).toBe(-51);
+  expect(functions.reverseInt(-90)).toBe(-9);
+});
+
+test("fizzBuzz", () => {
+  expect(
+    functions.fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+  ).toEqual([
+    1,
+    2,
+    "fizz",
+    4,
+    "Buzz",
+    "fizz",
+    7,
+    8,
+    "fizz",
+    "Buzz",
+    11,
+    "fizz",
+    13,
+    14,
+    "fizzBuzz"
+  ]);
+});
+
+test("chunk", () => {
+  expect(functions.chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+});
