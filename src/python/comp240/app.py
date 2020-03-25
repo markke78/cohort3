476d245fb672f9553220084efbb23825cc873cae
markke78 <56062146+markke78@users.noreply.github.com>
2020-03-25 02:24:21 -0600

    completed comp240

--- src/python/comp240/app.py
+++ src/python/comp240/app.py
@@ -1,73 +1,79 @@
 from flask import Flask, jsonify, request, render_template
+from read_xls import read_data
+from openpyxl import load_workbook, Workbook
 
 app = Flask(__name__)
 
-stores=[
-    {    
-        'name':'My Wonderful Store',
-        'items':[
-            {
-            'name':'My Item',
-            'price':15.99
-            }
-        ]
-    }
-]
+wb = load_workbook(filename="sampledata1.xlsx")
+wb_obj = read_data(wb)
+# {"wb_obj":
+# {"customer":[{"address":"123 Riverbend SE","customer_id":2,"email":"rachel@telus.ca","first_name":"Rachel","last_name":"Smith","phone":"403-324-3455"}],
+# "invoice":[{"GST(5%)":124.9985,"customer_id":1,"invoice_date":"12-01-2019","invoice_id":1,"sales_amount":2499.97,"total_amount":2624.9685}],
+# "product":[{"prod_id":1,"prod_name":"Dishwasher","prod_price":599.99}]}}
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
-#POST-used to receive data
-#Get-used to send data back only
 
-#POST/store data:{name}
-@app.route('/store', methods=['POST'])
-def create_store():
+# POST mothods
+@app.route("/", methods=["POST"])
+def creat_wb_obj_item():
     request_data = request.get_json()
-    new_store={
-        'name':request_data['name'],
-        'items':[]
+    wb_obj["invoice_detail"] = request_data["invoice_detail"]
+    return jsonify(wb_obj)
+
+
+@app.route("/customer", methods=["POST"])
+def create_customer():
+    request_data = request.get_json()
+    new_customer = {
+        "customer_id": request_data["customer_id"],
+        "address": request_data["address"],
+        "email": request_data["email"],
+        "first_name": request_data["first_name"],
+        "last_name": request_data["last_name"],
+        "phone": request_data["phone"],
     }
-    stores.append(new_store)
-    return jsonify(new_store)
+    wb_obj["customer"].append(new_customer)
+    return jsonify(new_customer)
 
-#GET/store/<string:name>
-@app.route('/store/<string:name>')#'http://127.0.0.1:5000/store/some_name'
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'message':'store not found'})
-    #Iterate over stores
-    #if the store andme matches, return it
-    #If none match, return an error message
 
-#GET/store
-@app.route('/store')
+@app.route("/customer/<int:id>", methods=["POST"])
+def create_item_on_customer(id):
+    request_data = request.get_json()
+    for index in wb_obj["customer"]:
+        if index["customer_id"] == id:
+            index["gender"] = request_data["gender"]
+            return jsonify(index)
+    return jsonify({"message": "customer not found"})
+
+
+# GET methods
+@app.route("/")
 def get_stores():
-    return jsonify({'stores':stores})
+    return jsonify(wb_obj)
 
-#POST/store/<string:name>/item{name:, price:}
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item={
-                'name':request_data['name'],
-                'price':request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message':'store not found'})
 
-#GET/store/<string:name>/item
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items':store['items']})
-    return jsonify({'message':'store not found'})
+@app.route("/customer")
+def get_customer():
+    return jsonify({"customer": wb_obj["customer"]})
 
-app.run(port=5000)
+
+@app.route("/customer/<string:address>")  #'http://127.0.0.1:5000/store/some_name'
+def get_customer_address(address):
+    for indx in wb_obj["customer"]:
+        if indx["address"] == address:
+            return jsonify(indx)
+    return jsonify({"message": "customer not found"})
+
+
+# #GET/store/<string:name>/item
+# @app.route('/store/<string:name>/item')
+# def get_item_in_store(name):
+#     for store in stores:
+#         if store['name'] == name:
+#             return jsonify({'items':store['items']})
+#     return jsonify({'message':'store not found'})
+
+app.run(port=5000, debug=True)
---
+++ src/python/comp240/read_xls.py
@@ -0,0 +1,165 @@
+from openpyxl import load_workbook, Workbook
+import json
+
+# # read excel sheet into memory
+def read_data(wb):
+    new_obj = {}
+    for sheets in wb:
+        new_list = []
+        new_obj[sheets.title] = new_list
+        headers = {}
+        for rowidx, row in enumerate(sheets):
+            item = {}
+            for cellidx, cell in enumerate(row):
+                if rowidx == 0:
+                    headers[cellidx] = cell.value
+                else:
+                    item[headers[cellidx]] = cell.value
+            if rowidx > 0:
+                new_list.append(item)
+
+    return new_obj
+
+
+try:
+    wb = load_workbook(filename="sampledata1.xlsx")
+    new_obj = open("new_obj.json", "w")
+    json.dump(read_data(wb), new_obj, indent="\t", separators=(",", ": "))
+finally:
+    new_obj.close()
+
+
+# {
+#     "customer": [
+#         {
+#             "customer_id": 1,
+#             "first_name": "Rachel",
+#             "last_name": "Smith",
+#             "address": "123 Riverbend SE",
+#             "phone": "403-324-3455",
+#             "email": "rachel@telus.ca",
+#         },
+#         {
+#             "customer_id": 2,
+#             "first_name": "Bobby",
+#             "last_name": "Smith",
+#             "address": "435 Crest Place NE",
+#             "phone": "587-321-4564",
+#             "email": "bobby@gmail.com",
+#         },
+#         {
+#             "customer_id": 3,
+#             "first_name": "Rebecca",
+#             "last_name": "Wang",
+#             "address": "231 Crest Place NE",
+#             "phone": "403-132-5798",
+#             "email": "rebecca@hotmail.com",
+#         },
+#         {
+#             "customer_id": 4,
+#             "first_name": "Tim",
+#             "last_name": "Cormier",
+#             "address": "435 Redstone NE",
+#             "phone": "403-432-4532",
+#             "email": "tim@gmail.com",
+#         },
+#         {
+#             "customer_id": 5,
+#             "first_name": "Phil",
+#             "last_name": "Murray",
+#             "address": "568 NoseHill",
+#             "phone": "587-091-0934",
+#             "email": "phil@shaw.ca",
+#         },
+#         {
+#             "customer_id": 6,
+#             "first_name": "Shane",
+#             "last_name": "James",
+#             "address": "987 Brightton SE",
+#             "phone": "587-765-7898",
+#             "email": "shane@gmail.com",
+#         },
+#         {
+#             "customer_id": 7,
+#             "first_name": "John",
+#             "last_name": "Sandhu",
+#             "address": "356 Willow Park",
+#             "phone": "587-321-2343",
+#             "email": "john@telus.ca",
+#         },
+#         {
+#             "customer_id": 8,
+#             "first_name": "Dave",
+#             "last_name": "Hynes",
+#             "address": "1213 Brightton SE",
+#             "phone": "403-554-2321",
+#             "email": "dave@hotmai.com",
+#         },
+#         {
+#             "customer_id": 9,
+#             "first_name": "Cristina",
+#             "last_name": "Davey",
+#             "address": "451 Kensington",
+#             "phone": "587-090-2931",
+#             "email": "cristina@gmail.com",
+#         },
+#         {
+#             "customer_id": 10,
+#             "first_name": "Dave",
+#             "last_name": "James",
+#             "address": "189 SunnySide",
+#             "phone": "403-432-7094",
+#             "email": "dave@shaw.ca",
+#         },
+#     ],
+#     "product": [
+#         {"prod_id": 1, "prod_name": "Dishwasher", "prod_price": 599.99},
+#         {"prod_id": 2, "prod_name": "Wine Cooler", "prod_price": 199.99},
+#         {"prod_id": 3, "prod_name": "Microwave", "prod_price": 159.99},
+#         {"prod_id": 4, "prod_name": "Mini Oven", "prod_price": 299.99},
+#         {"prod_id": 5, "prod_name": "Electric Stove", "prod_price": 899.99},
+#         {"prod_id": 6, "prod_name": "Gas Stove", "prod_price": 1119.99},
+#         {"prod_id": 7, "prod_name": "Blender", "prod_price": 129.99},
+#         {"prod_id": 8, "prod_name": "Portable Heater", "prod_price": 119.99},
+#         {"prod_id": 9, "prod_name": "Dining aTable Set", "prod_price": 899.99},
+#         {"prod_id": 10, "prod_name": "Washer", "prod_price": 999.99},
+#     ],
+#     "invoice": [
+#         {
+#             "invoice_id": 1,
+#             "customer_id": 1,
+#             "invoice_date": "12-01-2019",
+#             "sales_amount": 2499.97,
+#             "GST(5%)": 124.9985,
+#             "total_amount": 2624.9685,
+#         },
+#         {
+#             "invoice_id": 2,
+#             "customer_id": 2,
+#             "invoice_date": "12-05-2019",
+#             "sales_amount": 2019.97,
+#             "GST(5%)": 100.9985,
+#             "total_amount": 4745.937,
+#         },
+#         {
+#             "invoice_id": 3,
+#             "customer_id": 3,
+#             "invoice_date": "12-09-2019",
+#             "sales_amount": 1419.97,
+#             "GST(5%)": 70.9985,
+#             "total_amount": 3611.937,
+#         },
+#     ],
+#     "invoice_detail": [
+#         {"detail_id": 1, "invoice_id": 1, "prod_id": 1, "quantity": 1},
+#         {"detail_id": 2, "invoice_id": 1, "prod_id": 10, "quantity": 1},
+#         {"detail_id": 3, "invoice_id": 1, "prod_id": 9, "quantity": 1},
+#         {"detail_id": 4, "invoice_id": 2, "prod_id": 8, "quantity": 1},
+#         {"detail_id": 5, "invoice_id": 2, "prod_id": 9, "quantity": 1},
+#         {"detail_id": 6, "invoice_id": 2, "prod_id": 10, "quantity": 1},
+#         {"detail_id": 7, "invoice_id": 3, "prod_id": 4, "quantity": 1},
+#         {"detail_id": 8, "invoice_id": 3, "prod_id": 8, "quantity": 1},
+#         {"detail_id": 9, "invoice_id": 3, "prod_id": 10, "quantity": 1},
+#     ],
+# }
+
