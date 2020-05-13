0e4629e81ce95128ade8320f9497a3380a45274d
markke78 <56062146+markke78@users.noreply.github.com>
2020-05-12 22:48:53 -0600

    used java BigDecimal to practice simple interest calculator

--- src/java/practice/java-primitive-data-type-in-depth/src/com/in28minutes/primitive/datatypes/BiNumberRunner.java
+++ src/java/practice/java-primitive-data-type-in-depth/src/com/in28minutes/primitive/datatypes/BiNumberRunner.java
@@ -7,7 +7,7 @@ public class BiNumberRunner {
 		BiNumber numbers = new BiNumber(2, 3);
 		System.out.println(numbers.add());
 		System.out.println(numbers.multiply());
-		numbers.doubleValue();
+		numbers.doubleValue();// Double both numbers
 		System.out.println(numbers.getNumber1());
 		System.out.println(numbers.getNumber2());
 			
